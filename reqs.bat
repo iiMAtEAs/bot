195d43dbdbbf2e601964394091182d013d12fa1e
iiMAtEAs <102245975+iiMAtEAs@users.noreply.github.com>
2025-09-04 08:40:22 +0100

    Create reqs.bat

---
+++ reqs.bat
@@ -0,0 +1,8 @@
+pip install discord.py
+pip install cryptography
+pip install requests
+pip install opencv-python
+pip install numpy
+pip install pillow
+pip install pygetwindow
+pip install pyautogui
